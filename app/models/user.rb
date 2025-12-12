1740f22c7c54d2d2297923f7a7b00e7c69f5ec21
LucaPrevi0o <luca01.previati@edu.unife.it>
2025-12-12 13:29:02 +0100

    added User model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  has_secure_password
+  
+  has_one :cart, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  validates :role, inclusion: { in: %w[user admin] }, allow_nil: true
+  
+  before_validation :set_default_role, on: :create
+  
+  private
+  
+  def set_default_role
+    self.role ||= 'user'
+  end
+end
